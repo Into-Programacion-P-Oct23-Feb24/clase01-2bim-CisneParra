f8b2183a242bfe2f11c013fec64b33e7ac2db8c2
Cisne Parra <adparra2@utpl.edu.ec>
2023-12-06 15:24:15 -0500

    Cambios de la clase

--- ManejoCicloFor/src/paquete01/Ejemplo01.java
+++ ManejoCicloFor/src/paquete01/Ejemplo01.java
@@ -17,11 +17,11 @@ public class Ejemplo01 {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 10; i++) {
-            System.out.printf("%d\n", i);
+            System.out.printf("%d", i);
         }
         
         for (int i = 1; i < 11; i++) {
-            System.out.printf("%d\n", i);
+            System.out.printf("%d", i);
         }
 
     }
